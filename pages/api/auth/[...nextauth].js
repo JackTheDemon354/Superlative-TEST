f493fd0a3d438ca5582dd9e0d1ccd83c6acc48b4
Jack van Wijk <jack.vanwijk@hotmail.com>
2025-12-12 09:05:53 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,28 +1,25 @@
-import NextAuth from "@auth/nextjs";
-import GoogleProvider from "@auth/core/providers/google";
-import GitHubProvider from "@auth/core/providers/github";
-import DiscordProvider from "@auth/core/providers/discord";
-
+// pages/api/auth/[...nextauth].js
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from 'next-auth/providers/github'
+import DiscordProvider from 'next-auth/providers/discord'
 
 export const authOptions = {
-providers: [
-GoogleProvider({
-clientId: process.env.GOOGLE_CLIENT_ID,
-clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-}),
-GitHubProvider({
-clientId: process.env.GITHUB_ID,
-clientSecret: process.env.GITHUB_SECRET,
-}),
-DiscordProvider({
-clientId: process.env.DISCORD_CLIENT_ID,
-clientSecret: process.env.DISCORD_CLIENT_SECRET,
-}),
-],
-secret: process.env.NEXTAUTH_SECRET,
-// callbacks: customize tokens/sessions here if you want
-// session: { strategy: 'jwt' }, // default is jwt for Auth.js
-};
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+}
 
-
-export default NextAuth(authOptions);
+export default NextAuth(authOptions)
